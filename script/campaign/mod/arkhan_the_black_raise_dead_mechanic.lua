464d739b856e0e534d2f212f1501dc37ac882c47
im-mortal <im.mortal@me.com>
2021-05-10 07:29:25 +0300

    Initial commit
    Signed-off-by: im-mortal <im.mortal@me.com>

---
+++ script/campaign/mod/arkhan_the_black_raise_dead_mechanic.lua
@@ -0,0 +1,93 @@
+local dout = _G.dout or function(...) end
+
+STEPHEN_ARKHAN_RAISE_DEAD_MECHANIC = STEPHEN_ARKHAN_RAISE_DEAD_MECHANIC or {}
+local mod = STEPHEN_ARKHAN_RAISE_DEAD_MECHANIC
+mod.name = "Arkhan Raising Dead Mechanic"
+
+out(mod.name .. " script file loaded");
+dout(mod.name .. " script file loaded");
+
+mod.activated = 0; -- To run or not to run?
+
+-- Define dictionary containing units and properties to populate Raising Dead mercenary pool with
+-- To be expanded to cover modded units
+mod.units_to_add_globally = {
+	["vanilla"] = {
+		{ ["name"] = "wh2_dlc09_tmb_mon_dire_wolves",           ["count"] = 2, ["replenishment_chance"] = 1,    ["max_count"] = 4, ["max_replenishment"] = 1, ["level"] = 0, ["technology_required"] = "", ["partial_replenishment"] = false },
+		{ ["name"] = "wh2_dlc09_tmb_mon_fell_bats",             ["count"] = 2, ["replenishment_chance"] = 1,    ["max_count"] = 4, ["max_replenishment"] = 1, ["level"] = 0, ["technology_required"] = "", ["partial_replenishment"] = false },
+		{ ["name"] = "wh2_dlc09_tmb_inf_crypt_ghouls",             ["count"] = 2, ["replenishment_chance"] = 1,    ["max_count"] = 4, ["max_replenishment"] = 1, ["level"] = 0, ["technology_required"] = "", ["partial_replenishment"] = false },
+		{ ["name"] = "wh2_dlc09_tmb_inf_skeleton_warriors_0",   ["count"] = 2, ["replenishment_chance"] = 1,  ["max_count"] = 5,  ["max_replenishment"] = 1, ["level"] = 0, ["technology_required"] = "", ["partial_replenishment"] = false },
+		{ ["name"] = "wh2_dlc09_tmb_inf_skeleton_spearmen_0",   ["count"] = 1, ["replenishment_chance"] = 1, ["max_count"] = 3,  ["max_replenishment"] = 1, ["level"] = 0, ["technology_required"] = "", ["partial_replenishment"] = false }
+	}
+};
+
+function mod:stephen_arkhan_populate_mercenary_pools()
+	local faction_name = "wh2_dlc09_tmb_followers_of_nagash";
+
+	-- Main action
+	-- Iterating through all regions (provinces) first
+	local all_regions = cm:model():world():region_manager():region_list();
+	local t = mod.units_to_add_globally
+
+	for i = 0, all_regions:num_items() - 1 do
+		local region = all_regions:item_at(i);
+
+		-- Populating province mercenary pool
+		for i1, domain in pairs(t) do
+			for i2 in pairs(domain) do
+				local unit = domain[i2];
+				out(mod.name .. ": adding " .. unit.name .. " to " .. region:name() );
+				dout(mod.name .. ": adding " .. unit.name .. " to " .. region:name() );
+				cm:add_unit_to_province_mercenary_pool(
+					region,                     -- REGION_SCRIPT_INTERFACE
+					unit.name,                  -- unit
+					unit.count,                 -- count
+					unit.replenishment_chance,  -- replenishment chance
+					unit.max_count,             -- max units
+					unit.max_replenishment,     -- max per turn
+					unit.level,                 -- xp
+					faction_name,               -- faction restriction (is it really needed?)
+					"",                         -- subculture restriction
+					unit.technology_required,   -- tech restriction
+					unit.partial_replenishment  -- partial replenishment
+				);
+			end
+		end
+	end
+	
+	return true;
+end
+
+cm:add_first_tick_callback(
+	function()
+		out(mod.name .. ": FIRST TICK REGISTERED");
+		dout(mod.name .. ": FIRST TICK REGISTERED");
+		-- WTF is going on?
+		--mod:stephen_arkhan_populate_mercenary_pools(); -- should work unconditionally
+		if ( cm:is_new_game() == true or mod.activated == 0 ) then
+			out(mod.name .. ": populating mercenary pools for Followers of Nagash");
+			dout(mod.name .. ": populating mercenary pools for Followers of Nagash");
+			mod:stephen_arkhan_populate_mercenary_pools();
+			mod.activated = 1;
+		end
+	end
+)
+
+--------------------------------------------------------------
+----------------------- SAVING / LOADING ---------------------
+--------------------------------------------------------------
+cm:add_saving_game_callback(
+	function(context)
+		out(mod.name .. ": SAVING");
+		dout(mod.name .. ": SAVING");
+		cm:save_named_value("stephen_arkhan_raise_dead_mechanic", mod.activated, context);
+	end
+);
+
+cm:add_loading_game_callback(
+	function(context)
+		out(mod.name .. ": LOADING")
+		dout(mod.name .. ": LOADING");
+		mod.activated = cm:load_named_value("stephen_arkhan_raise_dead_mechanic", mod.activated, context);
+	end
+);

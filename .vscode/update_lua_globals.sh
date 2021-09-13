92a73a8c848ada442cf1891a69e9addf5495d869
im-mortal <im.mortal@me.com>
2021-09-13 03:16:04 +0300

    Use `update_lua_globals.sh` for config generation

--- .vscode/update_lua_globals.sh
+++ .vscode/update_lua_globals.sh
@@ -1,10 +1,43 @@
 #!/bin/env bash
-CUSTOM_VARS=(
-  "ardm"
-)
 
-CONFIG_FILE="script/.luacheckrc"
+CONFIG_FILE=".luacheckrc"
 LUA_VENDOR_FILES=".vscode/autocomplete"
+
+CUSTOM_VARS=()
+if [[ "${CUSTOM_LUA_GLOBALS}" ]]; then
+  for g in ${CUSTOM_LUA_GLOBALS}; do
+    CUSTOM_VARS+=("${g}")
+  done
+fi
+
+# get script arguments
+PARAMS=""
+
+while (( "$#" )); do
+  [[ $1 == --*=* ]] && set -- "${1%%=*}" "${1#*=}" "${@:2}"
+  case "$1" in
+    -g|--global)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        CUSTOM_VARS+=("$2")
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+
+# set positional arguments in their proper place
+eval set -- "$PARAMS"
 
 curl -sL --fail \
   https://gistcdn.githack.com/Egor-Skriptunoff/e4ab3bfc777faf4482a1b3f3ae19181b/raw/a39198cf62a52be7956abbff145a7fbf3f9a128a/show_globals.lua \
